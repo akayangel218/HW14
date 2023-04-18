5425f418de6b1ae04c443c891e79f677c2ddbff6
Kidüs Michael <kidusmichael08@gmail.com>
2023-04-18 16:31:40 -0700

    skeleton created

---
+++ km6134_hw14.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> compare(vector<int> h, int start, int end) {
+    //if 
+}
+
+vector<int> minmax(vector<int> n, int start, int end) {
+    if (n.size() == 1) {
+        return n;
+    }
+    else {
+        //compare(n)
+        //compare(n)
+    }
+}
+
+int main() {
+
+    vector<int> test;
+    return 0;
+}
